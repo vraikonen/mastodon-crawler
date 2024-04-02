eddaa0d1364a6ab3f73ba5d43f356d12956f31d8
Vračević Nikola <nikola.vracevic@stud.sbg.ac.at>
2024-04-02 16:57:39 +0200

    Unit test

---
+++ tests/test_mongodb_query.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+from utils.reading_config import reading_config_db_params
+from modules.mongodb_query import get_last_toot_id
+
+
+from unittest.mock import MagicMock
+
+def test_get_last_toot_id():
+    
+    # Go to your db from config file and find the "oldest" scraped tooth
+    oldest_toot_id = 106589544834980076
+    last_tooth = get_last_toot_id()
+    assert oldest_toot_id == last_tooth
